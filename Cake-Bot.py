52f6f3ed9551a1121cd9dd62fa0a0e911b466bf4
StevenBenentt <52074393+StevenBenentt@users.noreply.github.com>
2019-12-22 10:14:36 -0700

    Updated Cakebot.
    Cakebot now doesn't require a user to activate cakebot by message. Cakebot will automatically cycle through every server/guild that he is in seeing if any users have a cakeday. Added .github command as well. Plan on adding a function that will enable admins/privileged users to set desired channel for cakeday announcements.

--- Cake-Bot.py
+++ Cake-Bot.py
@@ -1,46 +1,29 @@
-import discord
 import asyncio
+import discord
 import datetime
 
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await cakedayloop()
 
 
 @client.event
-async def on_message(message):
-    await cakeday(message)
-    await version(message)
-    if message.content.startswith('.start'):
-        await message.channel.send("Started bot. Date time: " + datetime.datetime.today().strftime("%d-%m-%Y"))
-        x = message.guild.members
-        while True:
-            today = todaydate()
-            for member in x:
+async def cakedayloop():
+    while True:
+        today = todaydate()
+        for guild in client.guilds:
+            for member in guild.members:
                 j = member.joined_at
                 j = j.strftime("%m %d")
                 if j == today:
                     await announcement(member.name, member.id)
-            await asyncio.sleep(86400) #24 hours
-
-
-def todaydate():
-    today = datetime.datetime.now()
-    today = today.strftime("%m %d")
-    return today
-
+        await asyncio.sleep(86400) #24 hours
 
 @client.event
-async def announcement(name, ID):
-    channel = client.get_channel()
-    await channel.send(name + " is celebrating their cake day!!! <@" + str(ID) + "> " + "@everyone")
-
-
-@client.event
-async def cakeday(message):
+async def on_message(message):
     if message.author == client.user:
         return
 
@@ -49,6 +32,21 @@ async def cakeday(message):
         j = (monthconvert(j.strftime("%m")) + (dayconvert(j.strftime("%d"))) + j.strftime("%Y"))
         await message.channel.send("Your cake day is " + j + "!!")
 
+    if message.content.startswith(".github"):
+        await message.channel.send("https://github.com/StevenBenentt")
+
+
+@client.event
+async def announcement(name, ID):
+    channel = client.get_channel()
+    await channel.send(name + " is celebrating their cake day!!! <@" + str(ID) + "> " + "@everyone")
+
+
+
+def todaydate():
+    today = datetime.datetime.now()
+    today = today.strftime("%m %d")
+    return today
 
 def monthconvert(month): #01 - 12
     if month == "01":
@@ -88,12 +86,5 @@ def dayconvert(day): #0x - xx
         return day + "th "
 
 
-@client.event
-async def version(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('.version'):
-        await message.channel.send("Version: 1.0 ")
-
-client.run("")
+client.run('')
